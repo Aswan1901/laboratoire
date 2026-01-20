0251ba2e3fdd9a9823d7da8d244d016b6f4d28ab
Aswan1901 <aswan.josephmathieu1999@gmail.com>
2026-01-20 15:24:07 +0100

    create technician equipment

---
+++ laboratoire/src/Entity/Equipment.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\Type;
+use App\Repository\EquipmentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EquipmentRepository::class)]
+class Equipment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(enumType: Type::class)]
+    private ?Type $type = null;
+
+    #[ORM\Column]
+    private ?bool $available = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?Type
+    {
+        return $this->type;
+    }
+
+    public function setType(Type $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function isAvailable(): ?bool
+    {
+        return $this->available;
+    }
+
+    public function setAvailable(bool $available): static
+    {
+        $this->available = $available;
+
+        return $this;
+    }
+}
