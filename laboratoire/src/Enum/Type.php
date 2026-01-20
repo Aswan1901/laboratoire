6954a752cddc7fccf58131e71a28281787f7c33c
Aswan1901 <aswan.josephmathieu1999@gmail.com>
2026-01-20 15:02:47 +0100

    create sample entity + enums

--- laboratoire/src/Enum/Type.php
+++ laboratoire/src/Enum/Type.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Config;
+namespace App\Enum;
 
 enum Type: string
 {
     case BLOOD = 'BLOOD';
     case TISSUE = 'TISSUE';
     case URINE = 'URINE';
+
+    case GENERAL = 'GENERAL';
 }
